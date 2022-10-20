89adab828c6bc5a2effae6cc845ace160e60198d
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-20 09:18:36 +0200

    Validate likes_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,14 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true }
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 
-  def last_three_posts
-    posts.order('created_at desc').limits(3)
+  validates :likes_counter, numericality: { only_integer: true }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def five_recent_comments
+    comments.last(5).reverse
   end
 end
