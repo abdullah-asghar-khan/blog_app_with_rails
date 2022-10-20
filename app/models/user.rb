4e377a08f0ac37d13092a89369ccf41383bcd2f2
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-20 09:15:48 +0200

    Add validation to post counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
 
   validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true }
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 
   def last_three_posts
