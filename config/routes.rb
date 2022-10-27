8d8c96a60b6cd25e3ae9299b12e3f22f1d8c0d9d
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-27 10:50:53 +0200

    Update route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'likes/create'
+  # get 'likes/create
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resource :users
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create, :new]
-      resources :likes, only: [:create]
+      resources :likes, only: :create
     end
   end
   # get "/users/:id/posts/:id", to "posts#show"
