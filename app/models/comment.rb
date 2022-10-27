897b4d602bfc427ad492412f8b07f084ab53e8e1
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-27 11:04:43 +0200

    Fix errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,6 @@ class Comment < ApplicationRecord
   after_save :update_comments_counter
 
   def update_comments_counter
-    # post.comments_counter = post.comments.size
     post.increment!(:comments_counter
   end
 end
