4a124e78c51b0e6ee5b9fb377fb01b0df9f5ce1b
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-27 12:51:51 +0200

    Changes done linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
-
 end
