4c8f21a6b571443c3c05136dfac6de298ac2ded8
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-31 10:18:33 +0100

    Add shows index

--- spec/post_show_spec.rb
+++ spec/post_show_spec.rb
@@ -1,44 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "PostShow", type: :feature do
-  before :each do
+RSpec.describe 'Post Shows', type: :system do
+    before do
     driven_by(:rack_test)
-    @user = User.create(name: "Abdullah Khan", photo:"https://images.unsplash.com/photo-1664126409242-5390777fc5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", bio: "Rails developer")
-    @user2 = User.create(name: "Abdel Majid", photo:"https://images.unsplash.com/photo-1664126409242-5390777fc5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", bio: "React developer")
-
-    @post = Post.create(author: @user, title: "Post 1", text: "This is the post")    
-    Comment.create(author: @user, post: @post, text: "This is my first comment")
-    Comment.create(author: @user2, post: @post, text: "This is my second comment")
-    Like.create(post: @post, author: @user)
-    visit user_post_path(@user, @post)   
+    @user = User.create(name: 'Kim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @user2 = User.create(name: 'Arthur', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @post = Post.create(title: 'Hello', text: 'This is my first post', author: @user)
+    Comment.create(author: @user, post: @post, text: 'First comment')
+    Comment.create(author: @user2, post: @post, text: 'Second comment')
+    Like.create(post: @post, author: @user) 
   end
-
-  it 'Shows the posts title' do
+  it 'shows a posts title' do
+    visit user_post_path(@user, @post)
     expect(page).to have_content(@post.title)
   end
-
-  it 'Display the post author' do
+  it 'shows the posts author' do
+    visit user_post_path(@user, @post)
     expect(page).to have_content(@user.name)
   end
-
-  it 'I can see how many comments the post has' do
+  it 'shows the number of comments' do
+    visit user_post_path(@user, @post)
     expect(page).to have_content("Comments: #{@post.comments_counter}")
   end
-
-  it 'I can see how many likes the post has' do
+  it 'shows the number of likes' do
+    visit user_post_path(@user, @post)
     expect(page).to have_content("Likes: #{@post.likes_counter}")
   end
-
-  it 'I can see the post text' do
+  it 'shows a posts body' do
+    visit user_post_path(@user, @post)
     expect(page).to have_content(@post.text)
   end
-
-  it 'shows username of each comment author' do
+  it 'shows username' do
+    visit user_post_path(@user, @post)
     comment = @post.comments.first
     expect(page).to have_content(comment.author.name)
   end
-
-  it 'shows the comment each comment author left' do    
+  it 'shows the comment each comment author left' doit 'shows comments left' do
+    visit user_post_path(@user, @post)    
     comment = @post.comments.first
     comment2 = @post.comments.last
     expect(page).to have_content(comment.text)
