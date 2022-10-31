536bc23bfdd221307b002740d0ecf8a3fd292a59
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-31 09:48:25 +0100

    Add users index spec

--- spec/users_index_spec.rb
+++ spec/users_index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "UsersIndex", type: :feature do
     before :each do
-        @user = User.create(name: "Example User", photo:"", bio: "Lorem ipsum")
-        @user2 = User.create(name: "Example User2", photo:"", bio: "Lorem ipsum")
-        @user3 = User.create(name: "Example User3", photo:"", bio: "Lorem ipsum")
+        @user = User.create(name: "Abdullah Khan", photo:"https://images.unsplash.com/photo-1664126409242-5390777fc5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", bio: "Rails Developer")
+        @user2 = User.create(name: "Abdel Majid", photo:"https://images.unsplash.com/photo-1664126409242-5390777fc5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", bio: "Rails Developer")
+        @user3 = User.create(name: "John doe", photo:"https://images.unsplash.com/photo-1664126409242-5390777fc5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", bio: "Rails Developer")
     end
     it 'displays all users' do
         visit users_path
@@ -12,4 +12,20 @@ RSpec.describe "UsersIndex", type: :feature do
         expect(page).to have_content(@user2.name)
         expect(page).to have_content(@user3.name)
     end
+    it 'displays profile photo for each user' do
+        visit users_path
+        expect(page).to have_css("img[src*='https://images.unsplash.com/photo-1664126409242-5390777fc5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60']")
+    end
+    it 'displays number of posts for each user' do
+        visit users_path
+        expect(page).to have_content(@user.posts.count)
+        expect(page).to have_content(@user2.posts.count)
+        expect(page).to have_content(@user3.posts.count)
+    end
+    it 'redirects to user profile page when user name is clicked' do
+        visit users_path
+        click_link(@user.name)
+        expect(page).to have_current_path(user_path(@user.id))
+    end
+    
 end
