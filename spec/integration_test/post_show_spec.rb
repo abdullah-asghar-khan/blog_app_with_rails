6fd922c660ee50fb7b75ed80d87f85b621948daa
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-31 10:22:14 +0100

    Rewrite post controll

--- spec/integration_test/post_show_spec.rb
+++ spec/integration_test/post_show_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
-
 RSpec.describe 'Post Shows', type: :system do
-    before do
+  before do
     driven_by(:rack_test)
     @user = User.create(name: 'Kim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     @user2 = User.create(name: 'Arthur', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     @post = Post.create(title: 'Hello', text: 'This is my first post', author: @user)
     Comment.create(author: @user, post: @post, text: 'First comment')
     Comment.create(author: @user2, post: @post, text: 'Second comment')
-    Like.create(post: @post, author: @user) 
+    Like.create(post: @post, author: @user)
   end
   it 'shows a posts title' do
     visit user_post_path(@user, @post)
@@ -35,8 +34,8 @@ RSpec.describe 'Post Shows', type: :system do
     comment = @post.comments.first
     expect(page).to have_content(comment.author.name)
   end
-  it 'shows the comment each comment author left' doit 'shows comments left' do
-    visit user_post_path(@user, @post)    
+  it 'shows comments left' do
+    visit user_post_path(@user, @post)
     comment = @post.comments.first
     comment2 = @post.comments.last
     expect(page).to have_content(comment.text)
