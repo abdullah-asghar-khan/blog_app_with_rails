3339ef4d1e32a8c7da0ab0e9f031d7fdd6ffb821
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-01 11:02:11 +0100

    Config and run db migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+second_user = User.create(name: 'Lilly', email: 'lilly01@gmail.com', password: '@sam12345', photo: 'https://bit.ly/3BUPMZR', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Billy', email: 'billy01@gmail.com', password: '@edd14324', photo: 'https://bit.ly/3BUPMZR', bio: 'Teacher from Holand.')
