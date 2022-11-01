61b2443cd398d25981d84d2a6070485a1a185b30
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-01 11:11:33 +0100

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # get 'likes/create
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resource :users
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
@@ -11,5 +16,4 @@ Rails.application.routes.draw do
       resources :likes, only: :create
     end
   end
-  # get "/users/:id/posts/:id", to "posts#show"
 end
